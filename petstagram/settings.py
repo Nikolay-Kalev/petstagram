595e301adc7b2cccd93ddccd576938ce529ad3f1
Nikolay Kalev <manchesternik@gmail.com>
2024-11-18 00:50:44 +0200

    modify

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts.apps.AccountsConfig",
+    "pet.apps.PetConfig",
+    "photos.apps.PhotosConfig"
 ]
 
 MIDDLEWARE = [
